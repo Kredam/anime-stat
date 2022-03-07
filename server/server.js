416eb3c4b7114056dae14ccf19da9d5ba04a7a02
Kredam <akitlosz2000@gmail.com>
2022-03-07 20:00:22 +0100

    Local storgae done and design

--- server/server.js
+++ server/server.js
@@ -1,15 +1,60 @@
 const express = require("express");
 const axios = require("axios");
-const querystring = require('querystring')
 var cors = require("cors");
+const bodyparser = require('body-parser')
 const path = require('path');
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+const serviceAccount = require('../mal-readme-firebase-adminsdk-seybw-c9c3b94c50.json')
 require('dotenv').config({path: path.resolve('./.env')});
 
 const CLIENT_ID = process.env.REACT_APP_MAL_CLIENT_ID;
 const CLIENT_SECRET = process.env.REACT_APP_MAL_CLIENT_SECRET;
 const CODE_CHALLENGE = process.env.REACT_APP_CODE_CHALLENGE;
+
+let username = ""
 const app = express();
 app.use(cors({ credentials: true, origin: true }));
+app.use(bodyparser.json())
+// Initialize Firebase
+initializeApp({
+  credential: cert(serviceAccount)
+});
+const db = getFirestore()
+
+async function fetchUserInfo(user_access_token){
+  return await axios.get('https://api.myanimelist.net/v2/users/@me?', {
+    headers:{
+      'Content-Type' : 'application/x-www-form-urlencoded',
+      'Authorization':`Bearer ${user_access_token}`
+    }
+  }).then(res => {
+    return res.data.name
+  })
+}
+
+async function fetchMyAnimeListStats(access_token, username){
+  return await axios.get(`https://api.myanimelist.net/v2/users/${username}/animelist?`, {
+    params:{
+      status:"completed",
+      limit: 10,
+      sort:"list_score"
+    },
+    headers:{
+      'Content-Type' : 'application/x-www-form-urlencoded',
+      'Authorization':`Bearer ${access_token}`
+    }
+  }).then(res => {
+    return res.data
+  })
+}
+
+app.post("/animelist/stats", async(req, res) => {
+  const acces_token = (await db.collection("users").doc(req.body.username).get()).data().access_token
+  const stats = await fetchMyAnimeListStats(acces_token, req.body.username)
+
+  return res.send(stats)
+})
 
 app.get("/oauth/redirect", (req, res) => {
   const code = req.query.code;
@@ -22,17 +67,16 @@ app.get("/oauth/redirect", (req, res) => {
     headers: {
       'Content-Type':'application/x-www-form-urlencoded',  
     },
-  }).then((response) => {
-    console.log(response)
+  }).then(async(response) => {
+    username = await fetchUserInfo(response.data.access_token)
+    // const usersRef = db.collection("users").doc(username).set(response.data)
     res.redirect(
-      `http://localhost:3000?access_token=${response.data.access_token}`
-    );
-  }).catch(err => {
-    console.log()
+      `http://localhost:3000?username=${username}`
+      );
+    })
   });
-});
-
-const PORT = 8080;
-app.listen(PORT, () => {
-  console.log(`Listening at port ${PORT}`);
-});
+  
+  const PORT = 8080;
+  app.listen(PORT, () => {
+    console.log(`Listening at port ${PORT}`);
+  });
--- src/App.js
+++ src/App.js
@@ -1,31 +1,81 @@
-import { useEffect } from 'react';
-import './App.css';
+import {useEffect, useState} from 'react';
+import axios from 'axios'
+import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer } from 'recharts';
 
 function App() {
-  const client_id = process.env.REACT_APP_MAL_CLIENT_ID;  
+  const [logged, isLogged] = useState(false)
+  const client_id = process.env.REACT_APP_MAL_CLIENT_ID;
   const client_secret = process.env.REACT_APP_MAL_CLIENT_SECRET;
   const url = process.env.REACT_APP_URL;
   const code_challenge = process.env.REACT_APP_CODE_CHALLENGE;
+  const [animes, setAnimes] = useState([])
   console.log(String(code_challenge).length)
 
-  useEffect(() => {
-    const token = new URLSearchParams(window.location.search)
-    console.log(token)
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+
+  useEffect(() => { 
+    const username = new URLSearchParams(window.location.search).get("username")
+    console.log("logged value: " + logged)
+    if(localStorage.getItem("username")){
+      isLogged(() => true)
+    }
+    if(username !== null) {
+      localStorage.setItem("username", username)
+      console.log(localStorage.getItem("username"))
+      isLogged(() => true)
+    }
+    console.log(localStorage.getItem("username"))
+    axios({
+      method:"POST",
+      url:"http://localhost:8080/animelist/stats",
+      headers:{
+        'Content-Type' : 'application/json'
+      },
+      data:{
+        username:localStorage.getItem("username")
+      }
+    }).then((res) => {
+      setAnimes(() => res.data.data)
+          // console.log(JSON.stringify(res, null, 2))
+    })
   }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href={`${url}response_type=code&client_id=${client_id}&client_secret=${client_secret}&code_challenge=${code_challenge}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Register MAL
-        </a>
-      </header>
+    (!logged ?
+      <>
+        <div>
+          <a
+            className="App-link"
+            href={`${url}response_type=code&client_id=${client_id}&client_secret=${client_secret}&code_challenge=${code_challenge}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Register MAL
+          </a>
+        </div>
+      </>
+    :
+    <div class="table-div">
+      <table class="styled-table">
+        <thead>
+          <tr>
+            <th><img src='MyAnimeList_Logo.png' height={30} width={30} /></th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {animes.map(x => {
+            return <tr>
+              <td><img src={x.node.main_picture.medium}  width={30} height={40}/></td>
+              <td>{x.node.title}</td>
+            </tr>
+          })
+          }   
+        </tbody>
+      </table>
     </div>
+    )
   );
 }
 
